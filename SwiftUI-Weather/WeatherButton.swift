1af2793da690eec3c830b635107b2bdf6104f53f
davidcafor <davidcafor@gmail.com>
2023-11-28 19:39:48 +0100

    add views

---
+++ SwiftUI-Weather/WeatherButton.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-Weather
+//
+//  Created by David Cardeña on 28/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+        
+    }
+}
+
